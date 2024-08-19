f603549dababac644b357698cc8210f04442d1bf
Nathaniel Jonathan Rusli <19623113@std.stei.itb.ac.id>
2024-08-19 22:16:14 +0700

    feat: configured settings.py and urls.py to integrate project's apps

--- Elevate/Elevate/settings.py
+++ Elevate/Elevate/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users',
+    'todolist',
+    'subgoal_progresstracker',
+    'reminders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
