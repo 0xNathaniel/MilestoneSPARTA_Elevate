ff81439bdd7e8e14c7ceeaafe35366fe8965f7c6
Nathaniel Jonathan Rusli <19623113@std.stei.itb.ac.id>
2024-08-19 23:04:27 +0700

    fix: add quoatation mark to HttpResponseRedirect argument

--- Elevate/users/views.py
+++ Elevate/users/views.py
@@ -21,7 +21,7 @@ def login_view(request):
         # Check if authentication was successful
         if user is not None:
             login(request, user)
-            return HttpResponseRedirect(reverse(index))
+            return HttpResponseRedirect(reverse("index"))
         else:
             return render(request, "users/login.html")
         
