c1b979a0c0357a032a7cfa5af674a774d916113f
Nathaniel Jonathan Rusli <19623113@std.stei.itb.ac.id>
2024-08-19 22:39:04 +0700

    feat: add register, login, logout routing and page

--- Elevate/users/views.py
+++ Elevate/users/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "users/index.html")
+
+def register_view(request):
+    return render(request, "users/register.html")
+
+def login_view(request):
+    return render(request, "users/login.html")
+
+def logout_view(request):
+    return render(request, "users/logout.html")
