1b85769768d9904696f76e17d3eb4213c494d3c4
Nathaniel Jonathan Rusli <19623113@std.stei.itb.ac.id>
2024-08-19 22:52:40 +0700

    feat: add back end logic to user index, login, logout features

--- Elevate/users/views.py
+++ Elevate/users/views.py
@@ -1,14 +1,35 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def index(request):
+    # Redirect user to login page if not authenticated
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+    # Render index page if user is authenticated
     return render(request, "users/index.html")
 
-def register_view(request):
-    return render(request, "users/register.html")
-
 def login_view(request):
+    if request.method == "POST":
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication was successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse(index))
+        else:
+            return render(request, "users/login.html")
+        
     return render(request, "users/login.html")
 
 def logout_view(request):
-    return render(request, "users/logout.html")
+    logout(request)
+    return render(request, "users/login.html")
+
+def register_view(request):
+    return render(request, "users/register.html")
