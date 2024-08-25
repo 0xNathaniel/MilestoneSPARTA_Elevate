0d61dfa027b4b7c12431f8f52952f741e78a7345
Nathaniel Jonathan Rusli <19623113@std.stei.itb.ac.id>
2024-08-25 12:19:21 +0700

    feat: add responsiveness to login and sign up page

--- Elevate/user/forms.py
+++ Elevate/user/forms.py
@@ -6,9 +6,9 @@ class SignUpUserForm(UserCreationForm):
     # Remove Password-based authentication field
     usable_password = None
     
-    email = forms.EmailField(widget=forms.EmailInput(attrs={"class":"form-control"}))
-    first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
-    last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"class":"form-control form-control-size"}))
+    first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={"class":"form-control form-control-size"}))
+    last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={"class":"form-control form-control-size"}))
     
     class Meta:
         model = User    
@@ -17,6 +17,6 @@ class SignUpUserForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(SignUpUserForm, self).__init__(*args, **kwargs)
         
-        self.fields["username"].widget.attrs['class'] = 'form-control'
-        self.fields["password1"].widget.attrs['class'] = 'form-control'
-        self.fields["password2"].widget.attrs['class'] = 'form-control'
+        self.fields["username"].widget.attrs['class'] = 'form-control form-control-size'
+        self.fields["password1"].widget.attrs['class'] = 'form-control form-control-size'
+        self.fields["password2"].widget.attrs['class'] = 'form-control form-control-size'
