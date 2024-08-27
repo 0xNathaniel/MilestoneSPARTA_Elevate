327650897bcdd74086e4b63495d3debfa7536876
Nathaniel Jonathan Rusli <19623113@std.stei.itb.ac.id>
2024-08-27 16:07:59 +0700

    feat: add admin configuration

--- Elevate/todo/admin.py
+++ Elevate/todo/admin.py
@@ -2,3 +2,7 @@ from django.contrib import admin
 from .models import Tasks
 
 # Register your models here.
+class TasksAdmin(admin.ModelAdmin):
+    list_display = ("title", "created_at")
+    
+admin.site.register(Tasks, TasksAdmin)
