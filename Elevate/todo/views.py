1263aa3eef3890729f8b1bd22e1e0378cbc56616
Nathaniel Jonathan Rusli <19623113@std.stei.itb.ac.id>
2024-08-27 15:35:11 +0700

    feat: add to do list app functionality and style

--- Elevate/todo/views.py
+++ Elevate/todo/views.py
@@ -1,5 +1,25 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from .models import Tasks
 
 # Create your views here.
 def home(request):
-    return render(request, "todo/home.html")
+    tasks = Tasks.objects.filter(user=request.user).order_by('-created_at')
+    
+    return render(request, "todo/home.html", {
+        "tasks": tasks
+    })
+
+def add_task(request):
+    if request.method == "POST":
+        task = request.POST["task"]
+        if task:
+            Tasks.objects.create(user=request.user, title=task.capitalize())
+    return HttpResponseRedirect(reverse("todo:home"))
+
+def delete_task(request, task_id):
+    if request.method == "POST":
+        task = Tasks.objects.get(user=request.user, id=task_id)
+        task.delete()
+        return HttpResponseRedirect(reverse("todo:home"))
