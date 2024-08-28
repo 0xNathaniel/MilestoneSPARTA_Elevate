a0ebb4c5f9b3c01755680d368552a2783391976b
Nathaniel Jonathan Rusli <19623113@std.stei.itb.ac.id>
2024-08-28 13:58:08 +0700

    feat: configure reminder models to Admin

--- Elevate/reminder/admin.py
+++ Elevate/reminder/admin.py
@@ -2,3 +2,7 @@ from django.contrib import admin
 from .models import Reminder
 
 # Register your models here.
+class ReminderAdmin(admin.ModelAdmin):
+    list_display = ("title", "description", "date", "completed", "created_at")
+    
+admin.site.register(Reminder, ReminderAdmin)
